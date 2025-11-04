275af062f476933904e01e3ed0cb325b4babe104
Johnny Franjo <fjohnny@iinet.net.au>
2025-11-04 18:32:09 +1100

    project init

---
+++ server/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectToDatabase = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const connect = await mongoose.connect(process.env.MONGO_URI, {});
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+export default connectToDatabase;
+
+//hus9Jnoh3ksuetpv
